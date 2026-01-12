b7e904fbaac9d80488c8730da1224da010cda898
David Punkos <punkosdavid@gmail.com>
2026-01-11 23:46:09 -0500

    chore: remove test-publish folder and add to gitignore

--- test-publish/wwwroot/js/chart.js
+++
@@ -1,9 +0,0 @@
-import Chart from 'chart.js/auto';
-
-export function createChart(canvas, type, data, options) {
-    return new Chart(canvas, {
-        type: type,
-        data: data,
-        options: options
-    });
-} 
